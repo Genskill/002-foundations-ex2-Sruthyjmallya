c7e320fa596cbdf7e9336b322ffe69b056deef62
Sruthy J Mallya <53861843+Sruthyjmallya@users.noreply.github.com>
2021-05-21 15:22:03 +0530

    Create pythagorean.c

---
+++ pythagorean.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+  int side1,side2,side3;
+  printf("side 1 ");
+  scanf("%d",&side1);
+  printf("side 2 ");
+  scanf("%d",&side2);
+  printf("side 3 ");
+  scanf("%d",&side3);
+  
+  if(side1>side2 && side1>side3){
+    if(pow(side1,2)==(pow(side2,2)+pow(side3,2))){
+      printf("Yes");
+    }
+    else{
+      printf("No");
+    }
+  }
+  else if(side2>side3){
+    if(pow(side2,2)==(pow(side1,2)+pow(side3,2))){
+      printf("Yes");
+    }
+    else{
+      printf("No");
+    }
+  }
+  else{
+    if(pow(side3,2)==(pow(side2,2)+pow(side1,2))){
+      printf("Yes");
+    }
+    else{
+      printf("No");
+    }
+  }
+}
