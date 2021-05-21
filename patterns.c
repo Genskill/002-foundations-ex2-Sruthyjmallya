cd85b63288620ad4edf8f38977af0557eb3a5628
Sruthy J Mallya <53861843+Sruthyjmallya@users.noreply.github.com>
2021-05-21 15:01:02 +0530

    Create patterns.c

---
+++ patterns.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+  int pattern,row;
+  scanf("%d",&pattern);
+  scanf("%d",&row);
+ if(pattern == 1){
+    for(int i=row;i>0;i--){
+       for(int j=0;j<i;j++){
+           printf("#");
+       }
+       if(i>1){
+           printf("\n");
+       }
+   }
+  }
+  else if(pattern == 2){
+    for(int i=0;i<row;i++){
+       for(int j=0;j<=row-i-1;j++){
+           printf(" ");
+       }
+       for(int j=0;j<=i;j++){
+           printf("#");
+       }
+       if(i<row-1){
+           printf("\n");
+       }
+ 
+   }
+  }
+  else{
+    printf("Invalid Option\n");
+  }
+}
