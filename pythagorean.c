4005581148b00042e484fb370e7e0231550743a0
Sruthy J Mallya <53861843+Sruthyjmallya@users.noreply.github.com>
2021-05-26 21:50:10 +0530

    Update pythagorean.c

--- pythagorean.c
+++ pythagorean.c
@@ -1,17 +1,27 @@
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
+int pow(int x,int y){
+  int power = 1;
+  for(int i=0;i<y;i++){
+    power *= x;
+  }
+  return power;
+}
 void main(){
-  double side1,side2,side3;
+  int side1,side2,side3;
+  int hyp,other;
   printf("side 1 ");
-  scanf("%lf",&side1);
+  scanf("%d",&side1);
   printf("side 2 ");
-  scanf("%lf",&side2);
+  scanf("%d",&side2);
   printf("side 3 ");
-  scanf("%lf",&side3);
+  scanf("%d",&side3);
   
   if(side1>side2 && side1>side3){
-    if(pow(side1,2)==(pow(side2,2)+pow(side3,2))){
+    hyp = pow(side1,2);
+    other = pow(side2,2)+pow(side3,2);
+    if(hyp == other){
       printf("Yes");
     }
     else{
@@ -19,7 +29,9 @@ void main(){
     }
   }
   else if(side2>side3){
-    if(pow(side2,2)==(pow(side1,2)+pow(side3,2))){
+    hyp = pow(side2,2);
+    other = pow(side1,2)+pow(side3,2);
+    if(hyp == other){
       printf("Yes");
     }
     else{
@@ -27,7 +39,9 @@ void main(){
     }
   }
   else{
-    if(pow(side3,2)==(pow(side2,2)+pow(side1,2))){
+    hyp = pow(side3,2);
+    other = pow(side1,2)+pow(side2,2);
+    if(hyp == other){
       printf("Yes");
     }
     else{
