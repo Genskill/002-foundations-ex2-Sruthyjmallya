89ca852660fc943aeea0c240e28e2d38732b35fa
Sruthy J Mallya <53861843+Sruthyjmallya@users.noreply.github.com>
2021-05-27 15:06:49 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -2,35 +2,32 @@
 #include<cs50.h>
 void main()
 {
-int option,rows,i,j;
-option=get_int("");
-if(option==1)
-{
-rows=get_int("");
-for(i=rows;i>0;i--)
-{
-for(j=0;j<i;j++)
-{
-printf("#");
+  int pattern,row,i,j;
+  pattern=get_int("");
+  row =  get_int("");
+  if(pattern == 1){
+    for(i=row;i>0;i--){
+      for(j=0;j<i;j++){
+        printf("#");
+      }
+      printf("\n");
+    }
+  }
+  else if(){
+    for(i=row;i>0;i--){
+      for(j=1;j<=row;j++){
+        if(j<i){
+          printf(" ");
+        }
+        else{
+          printf("#");
+        }
+      }
+      printf("\n");
+    }
+  }
+  else{
+    printf("Invalid Option");
+  }
 }
-printf("\n");
-}
-}
-else if(option==2)
-{
-rows=get_int("");
-for(i=rows;i>0;i--)
-{
-for(j=1;j<=rows;j++)
-{
-if(j>=i)
-printf("#");
-else
-printf(" ");
-}
-printf("\n");
-}
-}
-else
-printf("Invalid entry");
-}
+
